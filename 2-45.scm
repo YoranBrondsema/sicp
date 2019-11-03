190594fe58f48f00ba4eda707ff4a25ed6bccd20
Yoran Brondsema <yoran.brondsema@gmail.com>
2019-11-03 21:19:37 +0100

    Add 2-45

---
+++ 2-45.scm
@@ -0,0 +1,15 @@
+; a) Because it assumes that the operator is binary.
+
+(put 'sum deriv-sum)
+(put 'product deriv-product)
+
+(define (deriv-sum exp var)
+  (make-sum (deriv (addend exp) var)
+            (deriv (augend exp) var)))
+
+(define (deriv-product exp var)
+  (make-sum
+    (make-product (multiplier exp)
+                  (deriv (multiplicand exp) var))
+    (make-product (deriv (multiplier exp) var)
+                  (multiplicand exp))))
